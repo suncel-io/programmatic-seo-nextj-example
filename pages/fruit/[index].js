fe71a544c54682d78e4a620092419ba3fc265d5e
david <david.dumt@gmail.com>
2022-10-11 11:51:16 +0200

    first-commit

---
+++ pages/fruit/[index].js
@@ -0,0 +1,33 @@
+//    /fruit/[index].js
+import {fruitsData} from '../../data.js' 
+
+
+const fruitPage = ({fruitData})=> {
+    console.log(fruitData)
+    return(
+        <div key={fruitData.id} className="mx-auto mt-8 text-center">
+            <h1 className='text-4xl text-bold'>{fruitData.name} calories</h1>
+            <p>serving : {fruitData.serving}</p>
+            <p>calories : {fruitData.calories}</p>
+            <p>calories / 100g: {fruitData.calories_100g}</p>
+        </div>
+    )
+}
+
+export async function getStaticPaths() {
+    let paths = fruitsData.map((x)=>{
+      return{'params': {index: x.name}}
+    })  
+    return {
+      paths,
+      fallback: false
+    };
+  }
+
+  export async function getStaticProps(context) {
+    return {
+      props: {fruitData : fruitsData.find(e => e.name == context.params.index)},
+    }
+  }
+
+  export default fruitPage
