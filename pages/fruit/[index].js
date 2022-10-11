be8e06609a155aa944f875962726e4a6bf220e4e
nico <nico.rusig@gmail.com>
2022-10-11 12:21:02 +0200

    Format improvement

--- pages/fruit/[index].js
+++ pages/fruit/[index].js
@@ -1,33 +1,31 @@
 //    /fruit/[index].js
-import {fruitsData} from '../../data.js' 
+import { fruitsData } from "../../data.js";
 
-
-const fruitPage = ({fruitData})=> {
-    console.log(fruitData)
-    return(
-        <div key={fruitData.id} className="mx-auto mt-8 text-center">
-            <h1 className='text-4xl text-bold'>{fruitData.name} calories</h1>
-            <p>serving : {fruitData.serving}</p>
-            <p>calories : {fruitData.calories}</p>
-            <p>calories / 100g: {fruitData.calories_100g}</p>
-        </div>
-    )
-}
+const FruitPage = ({ fruitData }) => {
+  return (
+    <div key={fruitData.id} className='mx-auto mt-8 text-center'>
+      <h1 className='text-4xl text-bold'>{fruitData.name} calories</h1>
+      <p>serving : {fruitData.serving}</p>
+      <p>calories : {fruitData.calories}</p>
+      <p>calories / 100g: {fruitData.calories_100g}</p>
+    </div>
+  );
+};
 
 export async function getStaticPaths() {
-    let paths = fruitsData.map((x)=>{
-      return{'params': {index: x.name}}
-    })  
-    return {
-      paths,
-      fallback: false
-    };
-  }
+  let paths = fruitsData.map((x) => {
+    return { params: { index: x.name } };
+  });
+  return {
+    paths,
+    fallback: false,
+  };
+}
 
-  export async function getStaticProps(context) {
-    return {
-      props: {fruitData : fruitsData.find(e => e.name == context.params.index)},
-    }
-  }
+export async function getStaticProps(context) {
+  return {
+    props: { fruitData: fruitsData.find((e) => e.name == context.params.index) },
+  };
+}
 
-  export default fruitPage
+export default FruitPage;
